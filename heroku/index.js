39921e37b1a2cb7925458ffee7479ea2ceaaac24
Tr1pl3Six <42490655+Tr1pl3Six@users.noreply.github.com>
2020-10-19 13:46:01 -0400

    Update index.js

--- heroku/index.js
+++ heroku/index.js
@@ -45,9 +45,14 @@ app.post('/facebook', function(req, res) {
     return;
   }
 
+  error_log(req.body);
+  
   console.log('request header X-Hub-Signature validated');
   // Process the Facebook updates here
   received_updates.unshift(req.body);
+  
+  
+  
   res.sendStatus(200);
 });
 
